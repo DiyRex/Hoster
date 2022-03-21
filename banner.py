cede72f6be210df800dd9c1da5ea2d9d6fb5dff4
DiyRex <75021968+DiyRex@users.noreply.github.com>
2022-03-21 16:54:07 +0530

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,15 @@
+message = """
+        
+░█─░█ █▀▀█ █▀▀ ▀▀█▀▀ █▀▀ █▀▀█ 
+░█▀▀█ █──█ ▀▀█ ──█── █▀▀ █▄▄▀ 
+░█─░█ ▀▀▀▀ ▀▀▀ ──▀── ▀▀▀ ▀─▀▀
+
+ -- Host/SNI Checker Tool By DiyRex --
+"""
+
+def banner():
+    print(message,"\n")
+
+tick = "☑"
+cross = "☒"
+move = "↑↓"
